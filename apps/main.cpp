8a9bd7c101c5ad2cafd8505062f684ce4cd71ce9
Wojciech Orański <wojtekor9@gmail.com>
2023-12-18 19:16:07 +0100

    CMake works now. Really.

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 #include "Warcaby/Player/bot.h"
 #include "Warcaby/Game/game.h"
@@ -26,3 +26,4 @@ int main(){
     }
     return 0;
 }
+*/

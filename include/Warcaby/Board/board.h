0a86555013c85e001fc08e4176fea2f47c3f87b9
Dawid Pawliczek <93407779+dawidpawliczek4@users.noreply.github.com>
2023-12-20 20:39:53 +0100

    Zarys logiki gry (#7)
    * test1
    
    * Small refactor
    
    * restrukturyzacja i zarys logiki gry
    
    * ruch zaczyna biały
    
    * Rozpisanie metody update w klasie Game
    
    ---------
    
    Co-authored-by (resolving conflicts): Dominik Muc <dominik.m.muc@gmail.com>

--- include/Warcaby/Board/board.h
+++ include/Warcaby/Board/board.h
@@ -1,11 +1,19 @@
 #pragma once
 
 #include "Warcaby/types.h"
+#include <array>
 
 class Board{
-    private:
-    BoardArray array;
+
     public:
-    void setField(int x, int y, FieldType type);
-    BoardArray getBoard();
+    Board();
+    // setField bedzie aktualizowal plansze, czyli zmienial wartosc w array (gdy juz sie upewnimy ze ruch jest poprawny)
+    void setField(int x, int y, int type);
+
+    // getBoard zwraca aktualny stan planszy (plansza jest prywatna, wiec nie mozna jej zobaaczyc z zewnatrz)    
+    std::array<std::array<int, 8>, 8> getBoard();
+
+    private:    
+    // 0 = puste pole, 1 = bialy pionek, 2 = czarny pionek, 3 = biala damka, 4 = czarna damka
+    std::array<std::array<int, 8>, 8> board; 
 };
